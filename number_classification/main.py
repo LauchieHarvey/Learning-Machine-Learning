5be3b70e58b3823d534a34b463dbbae6db564ebf
LauchieHarvey <lauchieharvey@gmail.com>
2020-05-21 17:53:28 +1000

    Wrote foundation of code for neural net and for the training/tests

---
+++ number_classification/main.py
@@ -0,0 +1,66 @@
+# Data manipulation and interaction with neural network
+
+from neural_net.py import *
+import torch
+import torchvision
+from torchvision import transforms, datasets
+import torch.optim as optim
+
+train = datasets.MNIST('', train = True, download = True,
+					transform = transforms.Compose([transforms.ToTensor()]))
+
+test = datasets.MNIST('', train = False, download = True,
+					transform = transforms.Compose([transforms.ToTensor()]))
+
+
+X_train = torch.utils.data.DataLoader(train, batch_size = 10, shuffle = True)
+
+X_test = torch.utils.data.DataLoader(test, batch_size = 10, shuffle = True)
+
+# initialise an instance of the neural network
+neural_net = NeuralNetwork()
+
+# Sets the learning rate for the neural network
+optimiser = optim.Adam(neural_net.parameters(), lr = 0.001)
+
+EPOCHS = 3
+
+
+
+
+# Train the neural network epoch times
+for epoch in range(EPOCHS):
+	# For each batch
+	for images, labels in X_train:
+
+		# Reshape image to a flat tensor
+		image = image.view(1, 28*28)
+		# Forward pass of image
+		output = neural_net(image)
+
+		loss = F.nll_loss(output, labels)
+		# Backpropogate the loss
+		optimiser.zero_grad()
+		loss.backward()
+
+		# update weights and biases
+		optimiser.step()
+		print(loss)
+
+
+
+# Neural net is trained, now test against test dataset
+
+with torch.no_grad():
+	# The results of the tests
+	total = 0
+	correct = 0
+	for images, labels in X_test:
+		output = neural_net(images.view(1, 28*28))
+
+		# Get the predictions of the neural net
+		_, predicted = torch.max(output, 1)
+		total += labels.size(0)
+		correct += (predicted == labels).sum().item()
+	print(f"Accuracy = {100 * correct/total}%")
+
---
+++ number_classification/neural_net.py
@@ -0,0 +1,32 @@
+# My first neural network
+
+import torch
+import torch.nn as nn
+
+# The layers of this neural network have been hardcoded for simplicity
+class NeuralNetwork(nn.Module):
+
+	def __init__(self):
+		super().__init__()
+
+		# Input layer --> HL1, 28*28 neurons, one for each pixel
+		self.full_connections1 = nn.Linear(28*28, 64)
+		# HL1 --> HL2
+		self.full_connections2 = nn.Linear(64, 64)
+		# HL2 --> HL3
+		self.full_connections3 = nn.Linear(64, 64)
+		# HL3 --> output layer
+		self.full_connections4 = nn.Linear(64, 10)
+
+
+
+	def forward(self, X):
+		# Pass input through first layer and apply activ func to resutl
+		X = F.relu(self.full_connections1(X))
+
+		X = F.relu(self.full_connections2(X))
+
+		X = F.relu(self.full_connections3(X))
+		X = self.LogSoftmax(self.full_connections4(X))
+
+		return X
